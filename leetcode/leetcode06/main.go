43ec5509f8656c2fa88ec7d6fee63ca8684e9754
zhouwen <zhoujingwen147@163.com>
2022-03-24 14:15:46 +0800

    第k个最大的数

--- leetcode/leetcode06/main.go
+++ leetcode/leetcode06/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	nums := []int{3, 2, 1, 5, 6, 4}
+	largest := findKthLargest(nums, 2)
+	fmt.Println(largest)
+}
+
+func findKthLargest(nums []int, k int) int {
+	for i := 0; i < k; i++ {
+		for j := 0; j < len(nums)-i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j], nums[j+1] = nums[j+1], nums[j]
+			}
+		}
+	}
+	return nums[len(nums)-k]
+}
+
+func sortColors(nums []int) {
+	start := -1
+	end := len(nums)
+	for fastIndex := 0; fastIndex < end; fastIndex++ {
+		if nums[fastIndex] == 0 {
+			start += 1
+			nums[start], nums[fastIndex] = nums[fastIndex], nums[start]
+		} else if nums[fastIndex] == 2 {
+			end -= 1
+			nums[end], nums[fastIndex] = nums[fastIndex], nums[end]
+		}
+	}
 }
