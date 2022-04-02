f210793118363000a7026f15f2c598f73a9f1b1d
zhouwen <zhoujingwen147@163.com>
2022-04-02 22:00:37 +0800

    二叉树前序遍历非递归写法

---
+++ leetcode/leetcode42/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	stack2 "awesomeProject/algorithm/dfs/dfs01/stack"
+	"fmt"
+)
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+二叉树的前序遍历 非递归写法
+*/
+func preorderTraversal2(root *TreeNode) []int {
+	res := make([]int, 0)
+	if root == nil {
+		return res
+	}
+
+	stack := stack2.NewStack()
+	node := root
+
+	for !stack.Empty() || node != nil {
+		for node != nil {
+			res = append(res, node.Val)
+			stack.Push(node)
+			node = node.Left
+		}
+		node = stack.Pop().(*TreeNode)
+		node = node.Right
+	}
+
+	return res
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return nil
+	}
+	ans := make([]int, 0)
+	stack := stack2.NewStack()
+	stack.Push(root)
+
+	for !stack.Empty() {
+		e := stack.Pop().(*TreeNode)
+		ans = append(ans, e.Val)
+
+		if e.Right != nil {
+			stack.Push(e.Right)
+		}
+		if e.Left != nil {
+			stack.Push(e.Left)
+		}
+	}
+
+	return ans
+}
+
+func CreateTree() *TreeNode {
+	root := TreeNode{Val: 1}
+	node2 := TreeNode{Val: 2}
+	node3 := TreeNode{Val: 3}
+
+	node4 := TreeNode{Val: 4}
+	node5 := TreeNode{Val: 5}
+	node6 := TreeNode{Val: 6}
+
+	root.Left = &node2
+	root.Right = &node3
+
+	node2.Right = &node4
+	node3.Left = &node5
+	node3.Right = &node6
+
+	return &root
+}
+
+func recoverFromPreorder(traversal string) *TreeNode {
+
+	return nil
+}
+
+func main() {
+	tree := CreateTree()
+	traversal := preorderTraversal2(tree)
+	fmt.Println(traversal)
+}
