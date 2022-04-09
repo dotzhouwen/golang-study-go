66a4aeff1156fa4e1975e058103d67adc1599a51
zhouwen <zhoujingwen147@163.com>
2022-04-09 17:16:53 +0800

    双指针 移动0

---
+++ leetcode/leetcode86/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+/**
+双指针 移动0
+*/
+func moveZeroes2(nums []int) {
+	slow := 0
+	for fast := 0; fast < len(nums); fast++ {
+		if nums[fast] != 0 {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			slow++
+		}
+	}
+}
+
+func moveZeroes(nums []int) {
+	slow := 0
+	fast := 0
+	for fast < len(nums) {
+		if nums[fast] != 0 {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	for i := slow; i < len(nums); i++ {
+		nums[i] = 0
+	}
+}
+
+func main() {
+	nums := []int{0, 1, 0, 3, 12}
+	moveZeroes2(nums)
+	fmt.Println(nums)
+}
