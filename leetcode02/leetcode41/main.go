06c6a54c626aa01c0ed7b3accac755dea3adfd69
zhouwen <zhoujingwen147@163.com>
2022-04-14 17:32:53 +0800

    最长无重复子串

--- leetcode02/leetcode41/main.go
+++ leetcode02/leetcode41/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 /**
 最长无重复子串
@@ -32,6 +35,28 @@ func check(arr []int) bool {
 		}
 	}
 	return true
+}
+
+func lengthOfLongestSubstring2(s string) int {
+	if s == "" {
+		return 0
+	}
+
+	chars := []rune(s)
+	charMap := make(map[rune]int, 128)
+
+	j := 0
+	ans := 0
+	for i := 0; i < len(chars); i++ {
+		v := chars[i]
+		charMap[v]++
+		for charMap[v] > 1 {
+			charMap[chars[j]]--
+			j++
+		}
+		ans = int(math.Max(float64(ans), float64(i-j+1)))
+	}
+	return ans
 }
 
 func main() {
